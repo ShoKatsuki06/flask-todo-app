d7c468fd53a9661bdae6274e7349557497458fb6
ShoKatsuki06 <shokatuki0603@gmail.com>
2021-07-24 01:36:59 +0900

    ver14

--- app.py
+++ app.py
@@ -38,7 +38,31 @@ dns = {
 #db = MySQL(**dns)
 #db = mysql.connector.connect(**dns)
 
+def selctcommand(dns,sql):
+     db = mysql.connector.connect(**dns)
+     cursor = db.cursor(buffered = True)
+     cursor.execute(sql)#上限は10個
+     rows = cursor.fetchall()
+     cursor.close()
+     db.close()
+     posts = Post.query.all()
+     return posts()
 
+def sqlcommand(dns,sql):
+    db = mysql.connector.connect(**dns)
+    cursor = db.cursor(buffered = True)
+    cursor.execute(sql)
+    db.commit()
+    cursor.close()
+    db.close()
+
+def sqlcommand1(dns,sql,id):#値の指定がある場合はこちらのメソッドを使います
+    db = mysql.connector.connect(**dns)
+    cursor = db.cursor(buffered = True)
+    cursor.execute(sql)
+    db.commit()
+    cursor.close()
+    db.close()
 
 #cursor = db.cursor(buffered=True)
 #cursor.execute("USE heroku_fab7e2e9408003b")
@@ -61,12 +85,13 @@ def dated_url_for(endpoint, **values):
 @app.route("/",methods = ["GET","POST"])
 def index():
     if request.method == "GET":
-       db = mysql.connector.connect(**dns)
-       cursor = db.cursor(buffered = True)
-       cursor.execute('SELECT * FROM todo LIMIT 10;')#上限は10個
-       rows = cursor.fetchall()
-       cursor.close()
-       db.close()
+       posts = selctcommand('SELECT * FROM todo LIMIT 10;')
+      #db = mysql.connector.connect(**dns)
+      # cursor = db.cursor(buffered = True)
+       #cursor.execute('SELECT * FROM todo LIMIT 10;')#上限は10個
+      # rows = cursor.fetchall()
+       #cursor.close()
+       #db.close()
        #posts = Post.query.all()
        return render_template("index.html", posts = rows )
     else:#登録
@@ -75,16 +100,17 @@ def index():
        due = request.form.get('due')
        print(due)
        sql = "INSERT INTO todo (title,detail,due ) VALUES (%s, %s, %s);"
-       db = mysql.connector.connect(**dns)
-       cursor = db.cursor(buffered = True)
-       cursor.execute(sql,(title,detail,due))
-       db.commit()
+       sqlcommand1(dns,sql,(title,detail,due))
+      # db = mysql.connector.connect(**dns)
+       #cursor = db.cursor(buffered = True)
+       #cursor.execute(sql,(title,detail,due))
+       #db.commit()
        #LINE送信
        sendText("TODOを登録しました\r\n{},{},{}".format(title,detail,due))
        #db.session.add(new_post)
        #db.session.commit()
-       cursor.close()
-       db.close()
+      # cursor.close()
+       #db.close()
        return redirect('/')
 
 #作成画面
@@ -94,13 +120,14 @@ def create():
 #詳細画面
 @app.route("/detail/<int:id>")
 def read(id):
-    db = mysql.connector.connect(**dns)
-    cursor = db.cursor(buffered = True)
-    cursor.execute('SELECT * FROM todo WHERE id = %s LIMIT 1',(id,) )
-    row = cursor.fetchone()
-    db.commit()
-    cursor.close()
-    db.close()
+    post = selctcommand('SELECT * FROM todo WHERE id = %s LIMIT 1',(id,))
+    #db = mysql.connector.connect(**dns)
+    #cursor = db.cursor(buffered = True)
+    #cursor.execute('SELECT * FROM todo WHERE id = %s LIMIT 1',(id,) )
+    #row = cursor.fetchone()
+    #db.commit()
+    #cursor.close()
+    #db.close()
     #post = Post.query.get(id)
     return render_template("detail.html", post = row)
 #削除
